d95f52896620d0d9708defd69bd2c36c711dfc19
Diego Feijo <diego.feijo@hotmail.com>
2019-12-17 17:47:44 -0200

    cap 1 e 2

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el:"#desafio",
+    data:{
+        nome: "Diego",
+        idade: 32,
+        img: "https://www.telegraph.co.uk/content/dam/news/2019/09/05/TELEMMGLPICT000208424999_trans_NvBQzQNjv4BqR9RRdoohtJOHZ1h3_dBTtHGeDGam_US7aEC3ZIaWb2U.jpeg?imwidth=700"
+    }
+})
