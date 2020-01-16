b36a610c175400a8cd65c1bd81ac361147e8542a
Diego Feijo <diego.feijo@hotmail.com>
2020-01-16 20:02:06 -0200

    avancos no curso

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-app>
+		<app-menu/>
+		<app-header/>
+		<app-gallery/>		
+		<app-footer/>		
+	</v-app>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+import Header from './Header.vue'
+import Gallery from './Gallery.vue'
+import Footer from './Footer.vue'
+
+export default {
+	components: {
+		'app-menu': Menu,
+		'app-header': Header,
+		'app-gallery': Gallery,
+		'app-footer': Footer
+		}	
+}
+</script>
